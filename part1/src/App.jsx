247fcc4cf24da1fcda34ce4e1603aadea16baa07
Robert C <carderober@gmail.com>
2024-12-17 00:34:13 -0300

    Exercise 1.12 to 1.14

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,68 +1,45 @@
 import { useState } from "react";
 
-const StatisticLine = ({ text, value }) => {
-  return (
-    <>
-      <tr>
-        <td>{text}</td>
-
-        <td>{value}</td>
-      </tr>
-    </>
-  );
-};
-
-const Statistics = ({ statistics }) => {
-  if (statistics.all === 0) {
-    return <p>No feedback given</p>;
-  }
-
-  return (
-    <>
-      <h1>statistics</h1>
-      <table>
-        <StatisticLine text="good" value={statistics.good} />
-        <StatisticLine text="neutral" value={statistics.neutral} />
-        <StatisticLine text="bad" value={statistics.bad} />
-        <StatisticLine text="all" value={statistics.all} />
-        <StatisticLine text="average" value={statistics.averageFinal} />
-        <StatisticLine text="positive" value={statistics.positive + "%"} />
-      </table>
-    </>
-  );
-};
-
 const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const [selected, setSelected] = useState(0);
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
 
-  const all = good + neutral + bad;
-  const average = good - bad / 3;
-  const averageFinal = average * 0.1;
-  const positive = (good / all) * 100;
+  const points = Array(anecdotes.length).fill(0);
+  const [countVotes, setCountVotes] = useState(points);
+  const [mostVoted, setMostVoted] = useState(0);
 
-  const statistics = {
-    good,
-    neutral,
-    bad,
-    all,
-    averageFinal,
-    positive,
+  const onHandleVote = () => {
+    const newCountVotes = [...countVotes];
+    newCountVotes[selected] += 1;
+    setCountVotes(newCountVotes);
+    console.log(newCountVotes);
+
+    const getHighestVoted = newCountVotes.indexOf(Math.max(...newCountVotes));
+    setMostVoted(getHighestVoted);
+  };
+
+  const onChangeAnecdote = () => {
+    setSelected(Math.floor(Math.random() * 8));
   };
 
   return (
     <>
-      <h1>give feedback</h1>
-      <button onClick={() => setGood((prevState) => prevState + 1)}>
-        good
-      </button>
-      <button onClick={() => setNeutral((prevState) => prevState + 1)}>
-        neutral
-      </button>
-      <button onClick={() => setBad((prevState) => prevState + 1)}>bad</button>
-
-      <Statistics statistics={statistics} />
+      <h1>Anecdotes</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>has {countVotes[selected]} votes</p>
+      <button onClick={onHandleVote}>vote</button>
+      <button onClick={onChangeAnecdote}>next anecdote</button>
+      <h1>Anecdote with most votes</h1>
+      <p>{anecdotes[mostVoted]}</p>
     </>
   );
 };
